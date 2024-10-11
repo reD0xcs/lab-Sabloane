7ff9f9108672ce8ad96c01e7fd662e503f024f8c
Andrei Diaconu <andrei.diaconu03@e-uvt.ro>
2024-10-11 08:45:21 +0300

    first push

---
+++ src/main/java/com/example/lab1sabloane/Lab1SabloaneApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1sabloane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1SabloaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1SabloaneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab1sabloane/Lab1SabloaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1sabloane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1SabloaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
